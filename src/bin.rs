bc796ed76d5d90898499a0413231b9788aafccf4
bananaboy139 <matteob319@gmail.com>
2022-06-01 19:08:04 +0200

    added checks

--- src/bin.rs
+++ src/bin.rs
@@ -3,12 +3,46 @@ fn main() {
 
 }
 
-struct Board([u8; (9*9)]);
-impl Board {
-	pub fn get(self, x: u8, y: u8) -> u8 {
+struct Game {
+	board : [u8; (9*9)]
+}
+impl Game {
+	pub fn get(&self, x: u8, y: u8) -> u8 {
 		let n = (y * 9) + x;
+		self.board[n as usize]
 	}
-	pub fn set(&self, x: u8, y: u8, set: u8) {
+	pub fn set(&mut self, x: u8, y: u8, set: u8) {
 		let n = (y * 9) + x;
+		self.board[n as usize] = set;
 	}
+	pub fn set_checked(&mut self, x: u8, y: u8, set: u8) {
+		Game::get(self, x, y);
+	}
+	pub fn check_line_x(&self, y: u8) -> Vec<u8> {
+		let line = y * 9;
+		let mut sum: Vec<u8> = Vec::new();
+		for x in 0..9 {
+			sum.push(self.board[(line + x) as usize]);
+		}
+		sum.sort();
+		sum
+	}
+	pub fn check_line_y(&self, x: u8) -> Vec<u8> {
+		let mut sum: Vec<u8> = Vec::new();
+		for y in 0..9 {
+			sum.push(self.board[(x + (9 * y)) as usize]);
+		}
+		sum.sort();
+		sum
+	}
+/* 	pub fn check_square(&self) -> Vec<u8> {
+		let mut sum: Vec<u8> = Vec::new();
+		for x in 0..3 {
+			for y in 0..3 {
+				sum.push(Game::get(self, x, y));
+			}
+		}
+		sum.sort();
+		sum
+	} */
 }
