aceb0bbd6ee52ee26c228ce7edbcb545a97a6feb
bananaboy319 <matteob319@gmail.com>
2022-06-03 20:08:36 +0200

    set checked

--- src/bin.rs
+++ src/bin.rs
@@ -17,22 +17,35 @@ impl Game {
 	}
 	pub fn set_checked(&mut self, x: u8, y: u8, set: u8) {
 		Game::get(self, x, y);
-	}
-	pub fn check_line_x(&self, y: u8) -> Vec<u8> {
-		let line = y * 9;
-		let mut sum: Vec<u8> = Vec::new();
-		for x in 0..9 {
-			sum.push(self.board[(line + x) as usize]);
+		let mut taken: [u8; (3 * 9)] = [0; (3 * 9)];
+		taken[0..9].copy_from_slice(&Game::check_line_x(self, y));
+		taken[9..(2 * 9)].copy_from_slice(&Game::check_line_y(self, x));
+		//taken[18..(3 * 9)].copy_from_slice(&Game::check_square(self, x, y));
+		match taken.iter().position(|&r| r == set) {
+			Some(n) => {
+				
+			},
+			None => {
+
+			}
+
 		}
-		sum.sort();
+	}
+	pub fn check_line_x(&self, y: u8) -> [u8; 9] {
+		let line = y * 9;
+		let mut sum: [u8; 9] = [0; 9];
+		for x in 0..9 {
+			sum[x] = self.board[(line + x as u8) as usize];
+		}
+		//sum.sort();
 		sum
 	}
-	pub fn check_line_y(&self, x: u8) -> Vec<u8> {
-		let mut sum: Vec<u8> = Vec::new();
+	pub fn check_line_y(&self, x: u8) -> [u8; 9] {
+		let mut sum: [u8; 9] = [0; 9];
 		for y in 0..9 {
-			sum.push(self.board[(x + (9 * y)) as usize]);
+			sum[y] = self.board[(x + (9 * y as u8)) as usize];
 		}
-		sum.sort();
+		//sum.sort();
 		sum
 	}
 /* 	pub fn check_square(&self) -> Vec<u8> {
