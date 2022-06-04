21e14dbbbdeb43a6fca4a33445d4dc3b0fcb0577
bananaboy139 <matteob319@gmail.com>
2022-06-04 17:23:47 +0200

    doc + fixed values + return missing values

--- src/bin.rs
+++ src/bin.rs
@@ -7,37 +7,43 @@ fn main() {
 type Position = (u8, u8);
 
 const WIDTH: usize = 9;
-const HEIGHT: usize = 9;
-const AREA: usize = (WIDTH * HEIGHT) as usize;
+const AREA: usize = (WIDTH * WIDTH) as usize;
 
 struct Game {
-	board : [u8; AREA]
+	board : [u8; AREA],
+	fixed: [bool; AREA] //mark numbers that cannot change
 }
 
 impl Game {
+	//TODO: implement import
 	pub fn new() -> Game {
-		Game {board : [0; AREA]}
+		Game {
+			board : [0; AREA],
+			fixed: [false; AREA]
+		}
 	}
-
+	///retrive value at box
 	pub fn get(&self, (x, y): Position) -> u8 {
 		let n = (y * WIDTH as u8) + x;
 		self.board[n as usize]
 	}
-
+	///set value at box, if changable
 	pub fn set(&mut self, (x, y): Position, set: u8) {
-		let n = (y * WIDTH as u8) + x;
-		self.board[n as usize] = set;
+		let n: usize = ((y * WIDTH as u8) + x) as usize;
+		if !self.fixed[n] { //check if mutable
+			self.board[n] = set;
+		}
 	}
-
+	//TODO: terrible function WIP
 	pub fn set_checked(&mut self, (x, y): Position, set: u8) -> Option<usize> {
 		Game::get(self, (x, y));
-		let mut taken: [u8; (3 * 9)] = [0; (3 * 9)];
-		taken[0..9].copy_from_slice(&Game::check_line_x(self, y));
-		taken[9..(2 * 9)].copy_from_slice(&Game::check_line_y(self, x));
-		taken[18..(3 * 9)].copy_from_slice(&Game::check_square(self, (x, y)));
+		let mut taken: [u8; (3 * WIDTH)] = [0; (3 * WIDTH)];
+		taken[0..WIDTH].copy_from_slice(&Game::check_line_x(self, y));
+		taken[9..(2 * WIDTH)].copy_from_slice(&Game::check_line_y(self, x));
+		taken[18..(3 * WIDTH)].copy_from_slice(&Game::check_square(self, (x, y)));
 		taken.iter().position(|&r| r == set)
 	}
-
+	///return horizontal line
 	pub fn check_line_x(&self, y: u8) -> [u8; WIDTH] {
 		let line = y * WIDTH as u8;
 		let mut sum: [u8; WIDTH] = [0; WIDTH];
@@ -46,19 +52,20 @@ impl Game {
 		}
 		sum
 	}
-
-	pub fn check_line_y(&self, x: u8) -> [u8; HEIGHT] {
-		let mut sum: [u8; HEIGHT] = [0; HEIGHT];
-		for y in 0..HEIGHT {
+	///return virtical line
+	pub fn check_line_y(&self, x: u8) -> [u8; WIDTH] {
+		let mut sum: [u8; WIDTH] = [0; WIDTH];
+		for y in 0..WIDTH {
 			sum[y as usize] = self.board[x as usize + (WIDTH * y)];
 		}
 		sum
 	}
-
+	//TODO: don't use position, make get square func and use square number instead
+	///return square WIP
 	pub fn check_square(&self, (x, y): Position) -> [u8; 9] {
 		let mut sum: [u8; 9] = [0; 9];
 		let mut n = 0;
-		for ny in (y - 1).min(0)..=(y + 1).max(HEIGHT as u8){
+		for ny in (y - 1).min(0)..=(y + 1).max(WIDTH as u8){
 			for nx in (x - 1).min(0)..=(x + 1).max(WIDTH as u8) {
 				sum[n] = Game::get(&self, (nx, ny));
 				n += 1;
@@ -66,4 +73,20 @@ impl Game {
 		}
 		sum
 	}
+	///return missing values
+	pub fn missing(&self, set: [u8; WIDTH]) -> Vec<u8>{
+		let mut missing: Vec<u8> = Vec::new();
+		for n in 1..=9 {
+			match set.iter().find(|&x| x == n) {
+				Some(i) => {
+					//do nothing
+				},
+				None => {
+					missing.push(n);
+				}
+			}
+		}
+		missing
+	}
+
 }
